c6a34e198f19c9d4f6178d0cb3a2a665519678be
ANDY PEÑA SAMPEDRO <ANDY PEA SAMPEDRO@ANDY>
2015-05-07 10:12:11 +0200

    Haciendo el commit de la clase vista

--- src/superficies/Vista.java
+++ src/superficies/Vista.java
@@ -6,9 +6,41 @@
 package superficies;
 
 /**
- *
+ *la clase vista es para imprimir en pantalla.
  * @author ANDY PEÑA SAMPEDRO
  */
 public class Vista {
-    
+    public void vista(Modelo mod){
+        /**
+         * Impresion por pantalla del cuadrado
+         *@param mod
+         */
+       if (mod.getShapeType().equalsIgnoreCase("square")) {
+          System.out.println("The area for your square: " + mod.getSquareArea());  
+        }
+       /**
+        * Impresion por pantalla del rectangulo
+        * @param mod
+        */
+        if (mod.getShapeType().equalsIgnoreCase("rectangle")) {
+            System.out.println("The area for your rectangle is: " + mod.getRectangleArea());
+                   }
+        /**
+         * Impresion por pantalla del triangulo
+         * @param mod
+         */
+        if (mod.getShapeType().equalsIgnoreCase("triangle")) {
+            System.out.println("Your triangles area is: " + mod.getTriangleArea());
+        }
+        /**
+         * Impresion por pantalla del circulo
+         * @param mod
+         */
+        if (mod.getShapeType().equalsIgnoreCase("circle")) {
+            System.out.println("Your Circles area is " + mod.getCircleArea());
+        }
+        
+        
+        
+    }
 }
